f9239064f5ff35c46aa8e626742d81d3f4ba878e
Morgan <120129256+notJunius@users.noreply.github.com>
2025-08-10 13:29:38 -0600

    Delete lua directory

--- lua/keymaps.lua
+++
@@ -1,6 +0,0 @@
-  vim.g.maplocalleader = "\\"
-  vim.g.mapleader = " "
-  vim.keymap.set('n', '<leader>w', ':w<CR>')
-  vim.keymap.set('n', '<leader>q', ':q!<CR>')
-  vim.keymap.set('n', '<leader>Q', ':qa<CR>')
-  vim.keymap.set('n', '<leader><leader>', ':lua vim.lsp.buf.format()<CR>')
