e7f356fc912c0ebe0c93a2f5bc9995c72852f387
notJunius <120129256+notJunius@users.noreply.github.com>
2025-09-08 15:37:56 -0600

    removed background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,7 +7,6 @@ return {
   -- Optional; default configuration will be used if setup isn't called.
   config = function()
     require("everforest").setup({
-	background = "medium",
 	transparent_background_level = 1,
     })
     vim.cmd("colorscheme everforest")
